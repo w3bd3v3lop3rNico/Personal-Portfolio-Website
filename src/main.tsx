0b6e4383dea3b2997a25af8f53673c7790afdbd7
Nicola <nicola.tabai98@gmail.com>
2024-08-21 19:15:44 +0200

    Create Header & Footer components, Create HomePage & AboutMe, modify main with Hash Routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createHashRouter, RouterProvider  } from 'react-router-dom'
 import './index.css'
+import HomePage from "./pages/HomePage"
+import AboutMe from './pages/AboutMe'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+
+  },
+  {
+    path: "/aboutme",
+    element: <AboutMe/>
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Header/>
+    <RouterProvider router={router}/>
+    <Footer/>
+  </React.StrictMode>,
 )
