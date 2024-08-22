f40eae31400c460f5e744c70e94bb7a44974f9da
Nicola <nicola.tabai98@gmail.com>
2024-08-22 11:39:25 +0200

    Styled navbar, add image, rm image from head in index.html

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,16 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          Logo
+          <img src="/not_found.jpg" alt="" width="30" height="24" />
+        </a>
+        <div
+          className="collapse navbar-collapse justify-content-center"
+          id="navbarNav"
+        >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link to="/" className="nav-link active" aria-current="page">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-link">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
