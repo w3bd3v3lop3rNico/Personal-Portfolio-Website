e49f44c7a2838fe34f504b3c6b96f68ecf87dfa4
Nicola <nicola.tabai98@gmail.com>
2024-08-22 10:30:56 +0200

    Add About & Home pages, create Navbar & Header components, Add Hash Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-
-import './App.css'
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Header from "./components/Header";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>My App</h1>
+      <HashRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </HashRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
